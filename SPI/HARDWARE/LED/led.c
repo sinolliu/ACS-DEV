8d88a892d15ab83de1d4ee9284ee5d4a21a5368c
sinolliu <aemidnight@gmail.com>
2017-04-19 00:01:36 +0800

    将源代码转换编码为UTF-8

--- SPI/HARDWARE/LED/led.c
+++ SPI/HARDWARE/LED/led.c
@@ -1,5 +1,35 @@
 #include "led.h"
 
 //////////////////////////////////////////////////////////////////////////////////	 
-//本程序只供学习使用，未经作者许可，不得用于其它任何用途
-//ALIENTEK mini
+//������ֻ��ѧϰʹ�ã�δ���������ɣ��������������κ���;
+//ALIENTEK mini�SSTM32������
+//LED��������	   
+//����ԭ��@ALIENTEK
+//������̳:www.openedv.com
+//�޸�����:2012/9/2
+//�汾��V1.0
+//��Ȩ���У������ؾ���
+//Copyright(C) �������������ӿƼ����޹�˾ 2009-2019
+//All rights reserved									  
+////////////////////////////////////////////////////////////////////////////////// 	   
+
+//��ʼ��PB5��PE5Ϊ������.��ʹ���������ڵ�ʱ��		    
+//LED IO��ʼ��
+void LED_Init(void)
+{
+ 
+ GPIO_InitTypeDef  GPIO_InitStructure;
+ 	
+ RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOD, ENABLE);	 //ʹ��PA,PD�˿�ʱ��
+	
+ GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;				 //LED0-->PA.8 �˿�����
+ GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		 //��������
+ GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		 //IO���ٶ�Ϊ50MHz
+ GPIO_Init(GPIOA, &GPIO_InitStructure);					 //�����趨������ʼ��GPIOA.8
+ GPIO_SetBits(GPIOA,GPIO_Pin_8);						 //PA.8 ������
+
+ GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;	    		 //LED1-->PD.2 �˿�����, ��������
+ GPIO_Init(GPIOD, &GPIO_InitStructure);	  				 //�������� ��IO���ٶ�Ϊ50MHz
+ GPIO_SetBits(GPIOD,GPIO_Pin_2); 						 //PD.2 ������ 
+}
+ 
